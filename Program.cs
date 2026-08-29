3f0074711b29b3eed9af168868c2facc07c582be
JCONDORHUATUCO <jodach34@gmail.com>
2026-08-28 22:22:14 -0500

    fix: configurar ruta principal a Solicitudes

--- Program.cs
+++ Program.cs
@@ -29,7 +29,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Solicitudes}/{action=Index}/{id?}")
+    pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 app.Run();
